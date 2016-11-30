fd15a3f1d6ca2e3069f26496d7d21956f2f6cebd
HakurouKen <ticshot@gmail.com>
2016-11-30 14:36:43 +0800

    Generator

---
+++ gulpfile.babel.js
@@ -0,0 +1,74 @@
+'use strict';
+
+import path from 'path';
+import fs from 'fs';
+import gulp from 'gulp';
+import { PluginError } from 'gulp-util';
+import rename from 'gulp-rename';
+import through from 'through2';
+
+// nodeppt 还没有 javascript Api, 这里直接使用内部库
+// nodeppt 内部用到了 `colors`, 但在 generate 文件中没有引入, 这里手工引入一下
+import 'colors';
+import generate from 'nodeppt/lib/generate';
+
+const nodepptPublish = (dest,isAll=true) => {
+    return through.obj(function(file, encoding, callback) {
+        let filepath = path.relative(file.cwd, file.path);
+        // generate 函数会在每次结尾直接输出 `生成结束 ` 等字样
+        // 这样会导致这里得到多个输出，这里直接 hack 掉 console
+        let _console = global.console.log;
+        global.console.log = function(){};
+        generate(filepath, dest, !!isAll);
+        // 只有第一个文件需要 release-all 剩下的复用即可
+        isAll = false;
+        global.console.log = _console;
+        return callback();
+    });
+};
+
+const remarkjsPublish = () => {
+    let tmpl = fs.readFileSync('./templates/remarkjs-template.html').toString();
+    return through.obj(function(file, encoding, callback) {
+        if (file.isNull()) {
+            return callback(null, file);
+        }
+        if (file.isStream()) {
+            this.emit('error', new PluginError('remarkjsPublish', 'Streams not supported!'));
+        } else if (file.isBuffer()) {
+            let markdown = file.contents.toString();
+            let filepath = path.relative(file.base,file.path);
+            let title = filepath.replace(/\.remark\.md/g,'').split(path.sep).join(' ');
+            file.contents = new Buffer(
+                tmpl
+                    .replace('{{TITLE}}',title)
+                    .replace('{{SOURCE}}',markdown)
+            );
+            return callback(null, file);
+        }
+    });
+};
+
+
+gulp.task('publish:nodeppt', () => {
+    gulp.src('src/*.nodeppt.md')
+        .pipe(nodepptPublish('publish/nodeppt'));
+});
+
+gulp.task('publish:remarkjs', () => {
+    gulp.src('src/**/*.remark.md')
+        .pipe(remarkjsPublish())
+        .pipe(rename({
+            extname: '.html'
+        }))
+        .pipe(gulp.dest('publish/remark/'))
+});
+
+gulp.task('publish:html', () => {
+    gulp.src('src/**/*.@(htm|html)')
+        .pipe(gulp.dest('publish'))
+});
+
+gulp.task('publish',['publish:nodeppt','publish:remarkjs','publish:html']);
+
+gulp.task('default',['publish']);
